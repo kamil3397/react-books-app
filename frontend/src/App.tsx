9a8b83e9a410c59a54824e7c6cec7547b46b63ac
kamil3397 <kkgolkiper33@gmail.com>
2025-07-30 22:35:42 +0200

    deleted unused useeffect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
 import { Outlet } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { CssBaseline } from '@mui/material'
-import { useEffect } from 'react'
-import axios from 'axios';
 
 const App = () => {
   
-  useEffect(() => {
-   
-    axios.post('')
-  }, [])
-
   return (
     <>
       <CssBaseline/>
